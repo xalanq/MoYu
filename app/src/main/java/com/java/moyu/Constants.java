3cbc02a21c679dbef3a7b018b18bb542a744252b
xalanq <xalanq@gmail.com>
2019-09-08 23:18:51 +0800

    Nothing

--- app/src/main/java/com/java/moyu/Constants.java
+++ app/src/main/java/com/java/moyu/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String DB_NAME = "Test_DB";
     public static final int DB_VERSION = 1;
     public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    public static final String APIUrl = "https://api2.newsminer.net/svc/news/queryNewsList";
+    public static final String APIUrl = "https://api2.domain.net/svc/news/queryNewsList"; // 请替换成你的API链接。
     public static final String UserAPIUrl = "https://moyu.xalanq.com/api";
     public static final int PAGE_SIZE = 15;
     public static final String[] category = {"社会", "娱乐", "体育", "科技", "军事", "教育", "文化", "健康", "财经", "汽车"};
